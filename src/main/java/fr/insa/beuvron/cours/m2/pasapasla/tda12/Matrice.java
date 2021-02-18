d159d865a1dfdf4cbfa90cf8dfb0498cd6261ac3
francois <francois@172.29.32.161>
2021-02-18 11:36:36 +0100

    apresTD2_A12

--- src/main/java/fr/insa/beuvron/cours/m2/pasapasla/tda12/Matrice.java
+++ src/main/java/fr/insa/beuvron/cours/m2/pasapasla/tda12/Matrice.java
@@ -12,47 +12,87 @@ import java.util.Arrays;
  * @author francois
  */
 public class Matrice {
-    
+
     private int nbrLig;
     private int nbrCol;
     private double[][] coeffs;
-    
-    public Matrice(int nl,int nc) {
+
+    public Matrice(int nl, int nc) {
         this.nbrLig = nl;
         this.nbrCol = nc;
         this.coeffs = new double[nl][nc];
-        for(int i = 0 ; i < this.nbrLig ; i ++) {
-            for(int j = 0; j < this.nbrCol ; j ++) {
+        for (int i = 0; i < this.nbrLig; i++) {
+            for (int j = 0; j < this.nbrCol; j++) {
                 this.coeffs[i][j] = 0;
             }
         }
     }
-    
+
     public String toString() {
         String s;
-        s= "";
-        for(int i = 0 ; i < this.nbrLig ; i ++) {
+        s = "";
+        for (int i = 0; i < this.nbrLig; i++) {
             s = s + "[";
-            for (int j = 0 ; j < this.nbrCol ; j++) {
-                s = s + String.format("%+4.2E",this.coeffs[i][j]);
+            for (int j = 0; j < this.nbrCol; j++) {
+                s = s + String.format("%+4.2E", this.coeffs[i][j]);
                 if (j != this.nbrCol - 1) {
-                    s =s  + " ";
+                    s = s + " ";
                 }
-                        
+
             }
             s = s + "]\n";
-            
+
         }
         return s;
     }
-    
-    public static void main(String[] args) {
-        int i = 3;
-        Matrice m = new Matrice(2,3);
-        m.coeffs[0][0] = 3.223456;
-        System.out.println("coeffs de m : " + 
-                Arrays.deepToString(m.coeffs));
-        System.out.println("m = \n" + m);
+
+    public static Matrice identite(int taille) {
+        Matrice m = new Matrice(taille, taille);
+        for (int i = 0; i < m.nbrLig; i++) {
+            m.coeffs[i][i] = 1;
+        }
+        return m;
     }
-    
+
+    public static int aleaUnOuDeux() {
+        return (int) (Math.random() * 2 + 1);
+    }
+
+    public static Matrice matAleaZeroUnDeux(int nbLig, int nbCol,
+            double probaZero) {
+        Matrice res;
+        res = new Matrice(nbLig, nbCol);
+        for (int i = 0; i < res.nbrLig; i++) {
+            for (int j = 0; j < res.nbrCol; j++) {
+                if (Math.random() > probaZero) {
+                    res.coeffs[i][j] = Matrice.aleaUnOuDeux();
+                }
+            }
+        }
+        return res;
+    }
+
+    public static void testAffiche() {
+        int i = 3;
+        Matrice m = new Matrice(2, 3);
+        m.coeffs[0][0] = 3.223456;
+        System.out.println("coeffs de m : "
+                + Arrays.deepToString(m.coeffs));
+        System.out.println("m = \n" + m);
+
+    }
+
+    public static void testIdentite() {
+        System.out.println("identité  4:");
+        System.out.println(Matrice.identite(4).toString());
+        System.out.println("identité 5 :");
+        System.out.println(Matrice.identite(5).toString());
+
+    }
+
+    public static void main(String[] args) {
+        // testAffiche();
+        testIdentite();
+    }
+
 }
